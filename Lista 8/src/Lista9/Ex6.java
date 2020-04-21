b260b2e633001f2bd44ac3b27f770f2ae721f206
HayatoG <guilherme.ryders@gmail.com>
2020-04-21 12:11:25 -0300

    Adicionando Ex6 - Lista 9

---
+++ Lista 8/src/Lista9/Ex6.java
@@ -0,0 +1,11 @@
+package Lista9;
+
+public class Ex6 {
+    public static void main(String[] args) {
+        int num = 0;
+        for(int i = 1; i <= 100; i++) {
+            System.out.println("Soma = " + num);
+            num += i;
+        }
+    }
+}
