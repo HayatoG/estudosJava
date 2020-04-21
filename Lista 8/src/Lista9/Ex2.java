1df77e6c2e23b03a253426f589834bad70450536
HayatoG <guilherme.ryders@gmail.com>
2020-04-21 11:42:59 -0300

    Adicionando Ex2 - Lista 9

---
+++ Lista 8/src/Lista9/Ex2.java
@@ -0,0 +1,9 @@
+package Lista9;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 1000; i=i+2){
+            System.out.println("Imprimindo: " + i);
+        }
+    }
+}
