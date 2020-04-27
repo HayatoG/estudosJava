d8d1c40d5506363d051269560a5510a8c3561700
HayatoG <guilherme.ryders@gmail.com>
2020-04-27 07:13:35 -0300

    Adicionando exercício 16 - Lista 2

---
+++ Lista 8/src/Lista2/Ex16.java
@@ -0,0 +1,12 @@
+package Lista2;
+
+public class Ex16 {
+    public static void main(String[] args) {
+        //27 dias do ano ele não joga
+        //338 dias por ano ele joga
+        int totalMin = 338 * 15;
+        int totalHoras = totalMin / 60;
+
+        System.out.print("Total de horas jogadas de Overwatch este ano: " + totalHoras + " horas.");
+    }
+}
