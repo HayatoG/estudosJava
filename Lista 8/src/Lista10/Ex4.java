b5c03286912ee59eb4d11e03952681ecebb9190f
HayatoG <guilherme.ryders@gmail.com>
2020-05-02 17:52:27 -0300

    Adicionando exercício 4 - Lista 10

--- Lista 8/src/Lista10/Ex4.java
+++ Lista 8/src/Lista10/Ex4.java
@@ -1,4 +1,22 @@
 package Lista10;
+import java.util.Scanner;
 
 public class Ex4 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int inteiro = 0;
+        int menor = inteiro;
+
+        System.out.printf("Digite um número inteiro: ");
+        menor = leitor.nextInt();
+
+        for (int i = 0; i < 4; i++) {
+            System.out.print("Digite um número inteiro: ");
+            inteiro = leitor.nextInt();
+            if (inteiro < menor){
+                menor = inteiro;
+            }
+        }
+        System.out.println("O menor número é: " + menor);
+    }
 }
